030ab296fbad35ba34e5174ac50b13d087d53194
김재형 <oozygreen@gmail.com>
2026-01-16 17:53:05 +0900

    404 에러 화면 출력 성공

--- server/server.py
+++ server/server.py
@@ -7,18 +7,47 @@ class MyRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
     # do_GET 의 이름은 BaseHTTPRequestHandler 클래스에서 미리 정의된 메서드 이름이므로 변경하면 안 됨
     # 클라이언트가 GET 요청을 보낼 때마다 이 메서드가 호출됨
-        self.send_response(200)
+
+        # ---------- 기본 서버 구동 로직 ----------
+        #self.send_response(200)
         # 200은 성공을 의미하는 HTTP 상태 코드
         # if문 등으로 요청 경로에 따라 다른 응답을 보내도록 확장할 수 있음
-        self.send_header('Content-type', 'text/html; charset=utf-8')
+        # self.send_header('Content-type', 'text/html; charset=utf-8')
         # 응답 헤더 설정, 여기서는 콘텐츠 타입을 HTML로 지정(UTF-8 인코딩 포함)
-        self.end_headers()
+        # self.end_headers()
         # 헤더 설정이 끝났음을 알림
-        message = "<h1>안녕하세요! 파이썬 서버입니다.</h1>"
+        # message = "<h1>안녕하세요! 파이썬 서버입니다.</h1>"
         # 응답 본문 설정 -> 현재 서버 구동 확인용으로 간결하게 작성했지만 추후 HTML 파일을 읽어와서 응답할 예정
-        self.wfile.write(message.encode('utf-8'))
+        # self.wfile.write(message.encode('utf-8'))
         # 응답 본문을 클라이언트로 전송, 문자열을 바이트로 인코딩하여 전송
+        # -----------------------------------------
 
+        # ---------- 클라이언트 UI 출력을 위한 고도화 및 404에러 예외처리 ----------
+        if self.path == '/':
+            file_path = 'client/index.html'
+        else:
+            file_path = 'client/404error.html'
+
+        content_type = 'text/plain'
+        if file_path.endswith('.html'):
+            content_type = 'text/html'
+
+        try:
+            # 파일 읽기 및 응답 전송
+            with open(file_path, 'rb') as f:
+                # 'rb' (read binary) 모드는 텍스트와 이미지 모두 처리가 가능
+                content = f.read()
+                
+                self.send_response(200)
+                self.send_header('Content-type', f'{content_type}; charset=utf-8')
+                self.end_headers()
+                self.wfile.write(content)
+                
+        except FileNotFoundError:
+            # 파일을 찾을 수 없을 때 404 에러 응답
+            self.send_error(404, f"File Not Found: {file_path}")
+
+        
     # POST가 필요하면 def do_POST(self):를 아래에 똑같이 정의해주면 됩니다. 메서드별로 함수를 따로 생성
 
 host = 'localhost' # 서버 호스트 주소 설정
